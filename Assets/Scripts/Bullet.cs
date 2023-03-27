c8d570fee63804dadb4cbaef75dfe90a27fce7dc
tachibanatowa <tachibana.towa@flaggs.co.jp>
2023-03-27 12:15:19 +0900

    playerの移動/玉の発射対応

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // 弾の発射スピード
+    public float Speed;
+    float uegenkai;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uegenkai = 5.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 下から上に弾を移動
+        transform.position += new Vector3(0, Speed, 0) * Time.deltaTime;
+
+        // 画面上方に消えたら弾を消去
+        if (transform.position.y >= uegenkai)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // プレイヤーの移動速度
+    public float _Speed ;
+
+    // 弾のプレハブ
+    public GameObject bulletPrefab;
+
+    ////上下左右の壁座標用
+    float _xleft;
+    float _xright;
+    float _yue;
+    float _yshita;
+    //上下左右の壁bool用
+    bool _xleftbool;
+    bool _xrightbool;
+    bool _yuebool;
+    bool _yshitabool;
+
+    //一定間隔での連射用
+    private float timeReset;
+    private float time;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //壁の数値を設定する
+        _yue = 4.75f ;
+        _yshita = -4.75f;
+        _xright = 2.7f;
+        _xleft = -2.7f;
+
+        //初期値設定
+        time = 0;
+        timeReset = 0.1f;
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = this.transform.position;
+
+        if (pos.y <= _yue)
+        {
+            _yuebool = true;
+        }
+        else
+        {
+            _yuebool = false;
+        }
+
+        if (pos.y >= _yshita)
+        {
+            _yshitabool = true;
+        }
+        else
+        {
+            _yshitabool = false;
+        }
+
+        ////
+        if (pos.x <= _xright)
+        {
+            _xrightbool = true;
+        }
+        else
+        {
+            _xrightbool = false;
+        }
+
+        if (pos.x >= _xleft)
+        {
+            _xleftbool = true;
+        }
+        else
+        {
+            _xleftbool = false;
+        }
+
+        if ((Input.GetKey(KeyCode.W)))
+        {
+            if (_yuebool)
+            {
+             pos.y =pos.y + _Speed;
+            }
+
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (_xleftbool)
+            {
+                pos.x = pos.x - _Speed;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (_yshitabool)
+            {
+                pos.y = pos.y - _Speed;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (_xrightbool)
+            {
+                pos.x = pos.x + _Speed;
+            }
+        }
+
+        // 現在位置にx,y の値を加算する
+        this.transform.position = pos;
+
+
+        //---銃の弾--
+        //一定時間で射出準備可能に
+        time += Time.deltaTime;
+        if (time > timeReset)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Instantiate(bulletPrefab, this.transform.position, Quaternion.identity);
+                time = 0;
+            }
+        }
+
+
+    }
+}
